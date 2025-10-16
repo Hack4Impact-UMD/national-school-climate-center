96d9e83a7b330829a9abada4792938674b08f4b1
Maiwand Raheem <114307104+MaiwandR@users.noreply.github.com>
2025-10-15 20:27:15 -0400

    Refactor App component layout and routes
    This is so that when rendered, the nav bar and the main page that is displayed aren't squeezed or compressed in any way
    
    In the initial version, this was the case, and the page being displayed was squashed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import NavBar from "./components/NavBar";
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className = "flex">
+    <div className="flex min-h-screen">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   )
 }
 
-
 export default App
+
