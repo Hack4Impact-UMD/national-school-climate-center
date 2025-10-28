b93b5cf765a23b76e3359492ca708e88dca4dcfa
isabellemtho <157723670+isabellemtho@users.noreply.github.com>
2025-10-28 14:40:22 -0400

    Feature: Nav Guards
    Added navigation guards based on role. Christian and Adam are "super admins" who have access to "Manage Users". All NSCC admins can access the "Survey Builder", "All Surveys", "Survey Analytics", and "General".

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ import Login from './pages/auth/Login'
 import Admin from './pages/Admin'
 import Layout from './components/Layout'
 import { Guard } from './components/ui/Guard'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import AccessDenied from '@/components/AccessDenied'
 
 
@@ -16,25 +13,6 @@ import AllSurveys from './pages/AllSurveys'
 import SurveyAnalytics from './pages/SurveyAnalytics'
 import General from './pages/General'
 import ManageUsers from './pages/ManageUsers' */
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-
-{/* import SurveyBuilder from './pages/SurveyBuilder'
-import AllSurveys from './pages/AllSurveys'
-import SurveyAnalytics from './pages/SurveyAnalytics'
-import General from './pages/General'
-import ManageUsers from './pages/ManageUsers' */}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 
 function App() {
@@ -49,19 +27,7 @@ function App() {
          <Route
            path="/surveybuilder"
            element={
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
              <Guard do="read" fallback={<AccessDenied />}>
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
                <SurveyBuilder />
              </Guard>
            }
@@ -71,19 +37,7 @@ function App() {
          <Route
            path="/allsurveys"
            element={
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
              <Guard do="read" fallback={<AccessDenied />}>
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in users only</p>}>
->>>>>>> Stashed changes
                <AllSurveys />
              </Guard>
            }
@@ -92,19 +46,7 @@ function App() {
          <Route
            path="/surveyanalytics"
            element={
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
              <Guard do="read" fallback={<AccessDenied />}>
-=======
-             <Guard do="read" fallback={<p>Signed-in only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="read" fallback={<p>Signed-in only</p>}>
->>>>>>> Stashed changes
                <SurveyAnalytics />
              </Guard>
            }
@@ -114,19 +56,7 @@ function App() {
          <Route
            path="/general"
            element={
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
              <Guard do="update" fallback={<AccessDenied />}>
-=======
-             <Guard do="update" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="update" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="update" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
                <General />
              </Guard>
            }
@@ -136,50 +66,18 @@ function App() {
          <Route
            path="/manageusers"
            element={
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
              <Guard do="manage_users" fallback={<AccessDenied />}>
-=======
-             <Guard do="manage_users" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="manage_users" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
-=======
-             <Guard do="manage_users" fallback={<p>Admins only</p>}>
->>>>>>> Stashed changes
                <ManageUsers />
              </Guard>
            }
          />
          */}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-         
+
+
          <Route
            path="/admin"
            element={
              <Guard do="manage_users" fallback={<AccessDenied />}>
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-
-         <Route
-           path="/admin"
-           element={
-             <Guard do="manage_users" fallback={<p>Admins only</p>}>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
                <Admin />
              </Guard>
            }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,45 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom'
 import { useAuth } from '@/contexts/AuthContext'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-import { Guard } from '@/components/ui/Guard'
-
->>>>>>> Stashed changes
-=======
-import { Guard } from '@/components/ui/Guard'
-
->>>>>>> Stashed changes
-=======
-import { Guard } from '@/components/ui/Guard'
-
->>>>>>> Stashed changes
 
 const NavBar: React.FC = () => {
    const { role } = useAuth()
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-   const canSeeSurvey = role === 'super_admin' || role === 'admin' || role === 'school_personnel'
-   const canSeeSettings = role === 'super_admin' || role === 'admin'
-
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
    return (
        <nav className="bg-primary text-primary-foreground min-h-screen w-80 pr-6">
        <div className = "flex items-center p-4">
@@ -61,14 +26,11 @@ const NavBar: React.FC = () => {
 
 
        <div className = "ml-5 text-white mt-28">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             <p className="font-body text-base text-body text-white">
                 Survey Creation
             </p>
             <div className = "mt-3 ml-7 text-lg mb-20 space-y-2">
-                <NavLink to="/surveybuilder" className = "flex items-center cursor-pointer justify-between">
+                <NavLink to="/surveybuilder" className = "flex items-center justify-between">
                     <h3 className="font-heading text-2xl text-heading text-white mb-1">
                         Survey Builder
                     </h3>
@@ -76,7 +38,7 @@ const NavBar: React.FC = () => {
                         ᐳ
                     </span>
                 </NavLink>
-                <NavLink to="/allsurveys" className = "flex items-center cursor-pointer justify-between">
+                <NavLink to="/allsurveys" className = "flex items-center justify-between">
                     <h3 className="font-heading text-2xl text-heading text-white mb-1">
                         All Surveys
                     </h3>
@@ -84,7 +46,7 @@ const NavBar: React.FC = () => {
                         ᐳ
                     </span>
                 </NavLink>
-                <NavLink to="/surveyanalytics" className = "flex items-center cursor-pointer justify-between">
+                <NavLink to="/surveyanalytics" className = "flex items-center justify-between">
                     <h3 className="font-heading text-2xl text-heading text-white">
                         Survey Analytics
                     </h3>
@@ -98,7 +60,7 @@ const NavBar: React.FC = () => {
                 Settings
             </p>
             <div className = "mt-3 ml-7 text-lg space-y-2">
-                <NavLink to="/general" className = "flex items-center cursor-pointer justify-between">
+                <NavLink to="/general" className = "flex items-center justify-between">
                     <h3 className="font-heading text-2xl text-heading text-white mb-1">
                         General
                     </h3>
@@ -106,7 +68,7 @@ const NavBar: React.FC = () => {
                         ᐳ
                     </span>
                 </NavLink>
-                <NavLink to="/manageusers" className = "flex items-center cursor-pointer justify-between">
+                <NavLink to="/manageusers" className = "flex items-center justify-between">
                     <h3 className="font-heading text-2xl text-heading text-white mb-1">
                         Manage Users
                     </h3>
@@ -115,92 +77,8 @@ const NavBar: React.FC = () => {
                     </span>
                 </NavLink>
             </div>
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-           {canSeeSurvey && (
-               <>
-                   <p className="font-body text-base text-body text-white">
-                       Survey Creation
-                   </p>
-                   <div className = "mt-3 ml-7 text-lg mb-20 space-y-2">
-                       <NavLink to="/surveybuilder" className = "flex items-center justify-between">
-                           <h3 className="font-heading text-2xl text-heading text-white mb-1">
-                               Survey Builder
-                           </h3>
-                           <span className="font-heading text-2xl text-white">
-                               ᐳ
-                           </span>
-                       </NavLink>
-                       <NavLink to="/allsurveys" className = "flex items-center justify-between">
-                           <h3 className="font-heading text-2xl text-heading text-white mb-1">
-                               All Surveys
-                           </h3>
-                           <span className="font-heading text-2xl text-white">
-                               ᐳ
-                           </span>
-                       </NavLink>
-                       <NavLink to="/surveyanalytics" className = "flex items-center justify-between">
-                           <h3 className="font-heading text-2xl text-heading text-white">
-                               Survey Analytics
-                           </h3>
-                           <span className="font-heading text-2xl text-white">
-                               ᐳ
-                           </span>
-                       </NavLink>
-                   </div>
-               </>
-           )}
-           {canSeeSettings && (
-               <>
-                   <p className="font-body text-base text-body text-white">
-                       Settings
-                   </p>
-                   <div className = "mt-3 ml-7 text-lg space-y-2">
-                       <NavLink to="/general" className = "flex items-center justify-between">
-                           <h3 className="font-heading text-2xl text-heading text-white mb-1">
-                               General
-                           </h3>
-                           <span className="font-heading text-2xl text-white">
-                               ᐳ
-                           </span>
-                       </NavLink>
-                       <Guard do="manage_users">
-                       <NavLink to="/manageusers" className = "flex items-center justify-between">
-                           <h3 className="font-heading text-2xl text-heading text-white mb-1">
-                               Manage Users
-                           </h3>
-                           <span className="font-heading text-2xl text-white">
-                               ᐳ
-                           </span>
-                       </NavLink>
-                       </Guard>
-                   </div>
-               </>
-           )}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
        </div>
      </nav>
    );
  };
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 export default NavBar;
-=======
-export default NavBar;
->>>>>>> Stashed changes
-=======
-export default NavBar;
->>>>>>> Stashed changes
-=======
-export default NavBar;
->>>>>>> Stashed changes
