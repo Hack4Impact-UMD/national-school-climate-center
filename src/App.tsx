99d6c85792e5cb26517541bb70384161edc6b030
Maiwand Raheem <maiwandraheem@Maiwands-MacBook-Air.local>
2025-10-30 21:49:22 -0400

    Fixed issues regarding app routing which was causing issues with page render and login and rerouted first sign in page ot welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
+// App.tsx (replace current <Routes> tree)
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './pages/auth/Login'
@@ -10,31 +11,34 @@ import AllSurveys from './pages/surveys/AllSurveys'
 import Layout from './components/Layout'
 import { ProtectedRoute } from './components/ProtectedRoute'
 
-function App() {
+export default function App() {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/login" element={<Login />} />
 
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+      {/* Authenticated shell */}
+      <Route element={<ProtectedRoute requireAuth />}>
+        <Route element={<Layout />}>
+          <Route index element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
 
-        <Route element={<ProtectedRoute requireAuth />}>
-          <Route path="/settings" element={<Settings />} />
-        </Route>
+          {/* Action-gated */}
+          <Route element={<ProtectedRoute requiredAction="create" />}>
+            <Route path="/surveys/builder" element={<SurveyBuilder />} />
+            <Route path="/surveys" element={<AllSurveys />} />
+            <Route path="/analytics" element={<Analytics />} />
+          </Route>
 
-        <Route element={<ProtectedRoute requiredAction="create" />}>
-          <Route path="/surveys/builder" element={<SurveyBuilder />} />
-          <Route path="/surveys" element={<AllSurveys />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Route>
-
-        <Route element={<ProtectedRoute requiredAction="manage_users" />}>
-          <Route path="/manage-users" element={<ManageUsers />} />
+          <Route element={<ProtectedRoute requiredAction="manage_users" />}>
+            <Route path="/manage-users" element={<ManageUsers />} />
+          </Route>
         </Route>
       </Route>
+
+      {/* Fallback */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
-
-export default App
