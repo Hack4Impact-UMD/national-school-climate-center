cfc20e7a26b7987165af744e8aeec1c4e714adcc
Maiwand Raheem <maiwandraheem@Maiwands-MacBook-Air.local>
2025-11-10 02:33:50 -0500

    fixed issue with home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,3 @@
-import { signOut } from 'firebase/auth'
-import { auth } from '@/firebase/config'
-
 export default function Home() {
   return (
     <div>
@@ -8,7 +5,6 @@ export default function Home() {
       <p className="text-gray-600 mb-6">
         This is the home page of your application.
       </p>
-      <button onClick={() => signOut(auth)}>Force Sign Out</button>
     </div>
   )
 }
