e9c483eaa70629ea77463a05c334242f61746a35
Debarshi Dey <ddey23@terpmail.umd.edu>
2026-03-01 23:51:22 -0500

    revised survey type for survey.ts

--- src/types/survey.ts
+++ src/types/survey.ts
@@ -1,24 +1,26 @@
-import type { Timestamp } from "firebase-admin/firestore"
+import type { FieldValue, Timestamp } from "firebase-admin/firestore"
+import type { Question } from "./surveybuilder"
 
-export type Survey = {
+export interface Survey {
   title: string
   description: string
   createdBy: string
-  createdAt: Timestamp
-  updatedAt: Timestamp
+  createdAt: Timestamp | FieldValue
+  updatedAt: Timestamp | FieldValue
   status: 'Draft' | 'Published' | 'Closed'
   visibility: 'School' | 'District' | 'Public'
   questionCount: number
   responseCount: number
-  schoolId: string
-  districtId: string
+  school_id: string
+  district_id: string
   tags: string[]
   type: 'Pulse' | 'Challenge' | 'Custom'
+  questions?: Question[] // optional for listing surveys without loading all questions, but should be included when fetching a single survey for editing or responding
 }
 
 export type PulseSurvey = Survey & {
-    school: string;
-    schoolDistrict: string;
+  school: string;
+  schoolDistrict: string;
 }
 
 export type ChallenegeSurvey = Survey & {
