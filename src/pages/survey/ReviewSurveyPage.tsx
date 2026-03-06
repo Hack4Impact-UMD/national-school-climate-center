6820c66cd3096a9440fff05d0501b7e0f45b3b04
davidbond <bonddwb@gmail.com>
2026-03-06 16:27:18 -0500

    fixed order bug

--- src/pages/survey/ReviewSurveyPage.tsx
+++ src/pages/survey/ReviewSurveyPage.tsx
@@ -42,8 +42,9 @@ export default function ReviewSurveyPage({
     setError(null);
 
     try {
-      const questionDocs = questions.map((q) => ({
+      const questionDocs = questions.map((q, index) => ({
         question_id: q.id,
+        order: index,
         text: q.prompt || q.name,
         questionType: q.questionType,
       }));
