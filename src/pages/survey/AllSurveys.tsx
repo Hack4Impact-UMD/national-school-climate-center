7c39e09a2ee039106eefc09bc084509822b2b2bf
Shakira Ali <alishakira690@gmail.com>
2026-03-24 18:08:00 -0400

    Fixed frontend design, fitlering table, and added button for creating surverys to page

--- src/pages/survey/AllSurveys.tsx
+++ src/pages/survey/AllSurveys.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function AllSurveys(){
 
- const [activeTab, setActiveTab] = useState("All Surveys");
-  const [searchQuery, setSearchQuery] = useState("");
+ const [activeTab, setActiveTab] = useState("All Surveys")
+ const [searchQuery, setSearchQuery] = useState("")
+   const navigate = useNavigate()
 
-  const filters = ["Name", "Respondent Group", "Compare By", "Survey Type", "From-To"];
-  const surveyTypes = ["All Surveys", "Pulse", "Challenge"];
+
+  const filters = ["School", "District", "State", "Number of Responses"]
+  const surveyTypes = ["All Surveys", "Pulse", "Challenge"]
 
   const testing = [
     {name: "GreenBelt", type: "Challenge", responses: 4990, lastResponse: "2 hours ago"},
@@ -14,25 +17,36 @@ export default function AllSurveys(){
     {name: "College Park", type: "Challenge", responses: 1223, lastResponse: "Yesterday"},
     {name: "Duvual", type: "Pulse", responses: 193, lastResponse: "6 mins ago"},
     {name: "Howard", type: "Challenge", responses: 23, lastResponse: "March 67, 2067"},
-  ];
+  ]
 
   const filteredSurveys = testing.filter((survey) => {
-    const matchesType = activeTab === "All Surveys" || survey.type === activeTab;
-    const matchesSearch = survey.name.toLowerCase().includes(searchQuery.toLowerCase());
-    return matchesType && matchesSearch;
-  });
+    const matchesType = activeTab === "All Surveys" || survey.type === activeTab
+    const matchesSearch = survey.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                          survey.type.toLowerCase().includes(searchQuery.toLowerCase()) ||
+                          survey.responses.toString().includes(searchQuery.toLowerCase()) ||
+                          survey.lastResponse.toLowerCase().includes(searchQuery.toLowerCase())
+    return matchesType && matchesSearch
+  })
 
   return (
     <div className="ml-8 mt-8">
-      <div className="flex justify-between items-center mb-8">
+      <div className="flex justify-between mb-4">
         <h1 className="text-4xl font-bold">All Surveys</h1>
+
+        <button
+        onClick={() => navigate("/surveys/builder")}
+        className="inline-flex items-center justify-center gap-2 px-6 py-2 bg-[#269ACF] text-white font-medium rounded-xl">
+        <span>{"Create New Survey "}</span>
+        </button>
       </div>
+
+      <h1 className="text-4xl mb-3 text-[#2F6CC0]">Filter</h1>
 
 
       <div className="flex flex-wrap gap-3 mb-8">
         {filters.map((filter) => (
           <div key={filter} className="flex flex-col">
-            <select className="bg-[#E9F9FF] rounded-lg px-4 py-2">
+            <select className="bg-[#E9F9FF] rounded-2xl px-4 py-2 mx-2">
               <option>{filter}</option>
             </select>
           </div>
@@ -46,20 +60,20 @@ export default function AllSurveys(){
             placeholder="Search" 
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="pl-4  py-2 border border-black rounded-lg "
+            className="pl-4 py-2 bg-[#E9F9FF] rounded-2xl"
           />
         </div>
 
 
-        <div className="flex flex-col mr-auto">
-          <div className="flex p-1 rounded-lg">
+        <div className="border border-[#2F6CC0] rounded-xl px-4 py-2">
+          <div className="flex p-1 rounded-lg text-[#269ACF]">
             {surveyTypes.map((type) => (
               <button
                 key={type}
                 onClick={() => setActiveTab(type)}
                 className={`px-4 py-1 rounded-lg ${
                   activeTab === type 
-                  ? "bg-white text-blue-600 shadow-sm" 
+                  ? "bg-[#2F6CC0] text-white" 
                   : ""
                 }`}
               >
@@ -71,9 +85,9 @@ export default function AllSurveys(){
       </div>
 
 
-
-      <div className="rounded-xl shadow-sm">
-        <table className="w-full text-left border-collapse">
+      <div className="bg-[#EAF8FD] p-8 rounded-xl">
+      <div className="rounded-xl shadow-lg bg-white">
+        <table className="w-full text-left border-collapse divide-y divide-gray-500">
           <thead>
             <tr>
               <th className="px-6 py-4">Name</th>
@@ -83,7 +97,7 @@ export default function AllSurveys(){
             </tr>
           </thead>
 
-          <tbody>
+          <tbody className="divide-y divide-gray-500">
             {filteredSurveys.length > 0 ? (
               filteredSurveys.map((survey) => (
                 <tr key={survey.name}>
@@ -115,6 +129,7 @@ export default function AllSurveys(){
         </table>
       </div>
     </div>
-  );
-};
+    </div>
+  )
+}
 
