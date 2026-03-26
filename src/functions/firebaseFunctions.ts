704af6e655a5ddb692b62950a5f85778dc6e1530
Ashlyn Wu <awu0730@terpmail.umd.edu>
2026-03-26 15:11:56 -0400

    ignoring updateSurvey for now

--- src/functions/firebaseFunctions.ts
+++ src/functions/firebaseFunctions.ts
@@ -13,13 +13,14 @@ export async function createSurvey(survey: Survey): Promise<DocumentReference> {
   return docRef
 }
 
-export async function updateSurvey(surveyId: string, updates: Partial<Survey>): Promise<void> {
-  const surveyRef = doc(db, 'surveys', surveyId)
-  await updateDoc(surveyRef, {
-    ...updates,
-    updatedAt: serverTimestamp(),
-  })
-}
+// we decided that updateSurvey and editSurvey are functionally the same, so we will just use edit. 
+// export async function updateSurvey(surveyId: string, updates: Partial<Survey>): Promise<void> {
+//   const surveyRef = doc(db, 'surveys', surveyId)
+//   await updateDoc(surveyRef, {
+//     ...updates,
+//     updatedAt: serverTimestamp(),
+//   })
+// }
 
 export async function saveSurvey(docRef: DocumentReference, survey: Survey) {
   await setDoc(docRef, {
